0689f96da2a9f4e1826459da3d9004e01e994214
ioshe <148945226+ioshe@users.noreply.github.com>
2023-12-27 23:39:05 +0900

    [Silver IV] Title: 문자열, Time: 40 ms, Memory: 31252 KB -BaekjoonHub

--- 백준/Silver/1120. 문자열/문자열.py
+++ 백준/Silver/1120. 문자열/문자열.py
@@ -1,12 +1,12 @@
 #https://www.acmicpc.net/problem/1120
 from sys import stdin
 a,b = stdin.readline().split()
-a = list(a)
-len_list = []
+result=51 # max _value
 for i in range(len(b) - len(a)+1) :
     hap = 0
     for j in range(len(a)) :
         if a[j] != b[i+j] :
             hap +=1
-    len_list.append(hap)
-print(min(len_list))
+    if result > hap:
+        result = hap
+print(result)
