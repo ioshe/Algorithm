a23af46892eb2f710684808561e4e8861ee9a620
ioshe <148945226+ioshe@users.noreply.github.com>
2024-01-07 23:33:17 +0900

    [Silver I] Title: 금민수의 개수, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1527. 금민수의 개수/금민수의 개수.py
+++ 백준/Silver/1527. 금민수의 개수/금민수의 개수.py
@@ -1,15 +1,15 @@
 #https://www.acmicpc.net/problem/1527
 MAX_LEN = 10 #1,000,000,000
-KEY_VALUE = ['4','7']
+KEY_VALUE = [4,7]
 result = [KEY_VALUE]
 for i in range(MAX_LEN) :
     temp = []
     for j in result[i] :
-        temp.append(j+"4")
-        temp.append(j+"7")
+        temp.append(j*10+4)
+        temp.append(j*10+7)
     result.append(temp)
 
-result = [int(item) for sublist in result for item in sublist]
+result = [item for sublist in result for item in sublist]
 
 
 a,b = map(int,input().split())
