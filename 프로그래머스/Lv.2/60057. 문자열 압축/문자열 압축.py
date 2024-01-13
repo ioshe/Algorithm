0d38cb4918f1865585dcee8e533255a826b3a3b8
ioshe <148945226+ioshe@users.noreply.github.com>
2024-01-13 17:34:51 +0900

    [level Lv.2] Title: 문자열 압축, Time: 0.03 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/Lv.2/60057. 문자열 압축/문자열 압축.py
+++ 프로그래머스/Lv.2/60057. 문자열 압축/문자열 압축.py
@@ -1,15 +1,9 @@
 def solution(s):
     answer = len(s)
     for i in range(1,len(s)//2+1) :
-        # if len(s) % i : 
-        #     continue
-        correct = []
-        for k in range(0,len(s),i) :
-            correct.append(s[k:k+i])
-        count = 0
-        j = 0
+        correct = [s[k:k+i] for k in range(0,len(s),i)]
+        count = j = 0
         while j < len(s)+1 :
-        #for j in range(0,len(s)+1,i) :
             temp_count = 0
             for l in range(j//i+1,len(correct)) :
                 if correct[l] == s[j:j+i] :
@@ -18,11 +12,6 @@ def solution(s):
                     break
             j += temp_count*i + i
             if temp_count > 0 :
-                #print(f'{i}일 때 count : {temp_count}')  
-                count += (temp_count*i) - len(str(temp_count+1))
-                #print(f'{temp_count}{s[j:j+1]}, {(temp_count*i) -1}')
-        #if count > 0 :
-            #print(f'{i}일 때 count : {count}')  
+                count += (temp_count*i) - len(str(temp_count+1)) 
         answer = min(answer,len(s) - count)
-    
     return answer
