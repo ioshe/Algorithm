f3fba957477f2ccca0ce282078621065e1b30575
ioshe <148945226+ioshe@users.noreply.github.com>
2024-01-16 00:26:19 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 56 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.py
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.py
@@ -1,49 +1,22 @@
+# https://www.acmicpc.net/problem/13251
 from sys import stdin
-
 n = int(stdin.readline())
 nums = list(map(int,stdin.readline().split()))
-#nums = list(map(int,stdin.readline().split()))
-operators_counts = list(map(int,stdin.readline().split()))
-#operators = list(map(int.stdin.readline().split()))
-operator_exp = ["+","-","*","//"]
-operators= [i for i in range(len(operators_counts)) for j in range(operators_counts[i])]
-
-top = float('-inf')
-low = float('inf')
+operators = list(map(int,stdin.readline().split()))
 result = []
-operateor_visited = []
-def dfs() :
-    global top,low
-    if len(operateor_visited) >= n-1 :
-        temp_re = nums[0]
-        #a = list(operator_exp[operators[i]] for i in operateor_visited)
-        #print(list(operator_exp[operators[i]] for i in operateor_visited))
-        for j in range(len(operateor_visited)) :
-            if operators[operateor_visited[j]] == 0 :
-                temp_re += nums[j+1]
-            elif operators[operateor_visited[j]] == 1 :
-                temp_re -= nums[j+1]
-            elif operators[operateor_visited[j]] == 2 :
-                temp_re *= nums[j+1]
-            else :
-                if temp_re < 0 :
-                    temp_re = -(abs(temp_re) // nums[j+1])
-                else : 
-                    temp_re //= nums[j+1]
-
-        if temp_re > top :
-            top = temp_re
-        if temp_re < low :
-            low = temp_re
-
-        return 
-    
-    for i in range(len(operators)) :
-        if i not in operateor_visited :
-            operateor_visited.append(i)
-            dfs()
-            operateor_visited.pop()
-
-dfs()
-print(top)
-print(low)
+def dfs(idx,plus,minus,multi,divide,sum) :
+    if idx >= n :
+        result.append(sum)
+        return    
+    else :
+        if plus :
+            dfs(idx+1,plus-1,minus,multi,divide,sum+nums[idx])
+        if minus :
+            dfs(idx+1,plus,minus-1,multi,divide,sum-nums[idx])
+        if multi :
+            dfs(idx+1,plus,minus,multi-1,divide,sum*nums[idx])
+        if divide :
+            dfs(idx+1,plus,minus,multi,divide-1,int(sum / nums[idx]))
+dfs(1,operators[0],operators[1],operators[2],operators[3],nums[0])
+print(max(result))
+print(min(result))
