5d575b0afd779830496239ba2a03587fb6bffaee
ioshe <148945226+ioshe@users.noreply.github.com>
2024-01-20 16:32:37 +0900

    [Gold IV] Title: 알파벳, Time: 5848 ms, Memory: 158884 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.py
+++ 백준/Gold/1987. 알파벳/알파벳.py
@@ -7,30 +7,21 @@ r,c = map(int,stdin.readline().split())
 map = [stdin.readline().rstrip() for _ in range(r)]
 
 # print(map)
-result = set()
-def dfs(i,j,visited,map,num) :
-    # for i in range(r) :
-    #     for j in range(c) :
-    #         print(map[i][j])
-    # if map[i][j] in visited :
-    #     result.add(len(visited))
-    #     print(visited)
-    #     return
-
-    # visited.append(map[i][j])
-
+result = 0
+def dfs(i,j,visited,map,num,result) :
     for (dx,dy) in [(-1,0),(0,-1),(1,0),(0,1)] :
         nx,ny = i+dx,j+dy
         if 0<=nx<r and 0<=ny<c and not(visited[ord(map[nx][ny]) - 65]) : # 'A' = 65
             visited[ord(map[nx][ny]) - 65] = 1
-            dfs(nx,ny,visited,map,num+1)
+            result = dfs(nx,ny,visited,map,num+1,result)
             visited[ord(map[nx][ny]) - 65] = 0
         else :
             #print(visited)
-            result.add(num)
+            if result < num :
+                result = num
 
+    return result
 visited = [0 for _ in range(26)]
 visited[ord(map[0][0])-65] = 1
-dfs(0,0,visited,map,1)
-print(max(result))
-
+result = dfs(0,0,visited,map,1,result)
+print(result)
