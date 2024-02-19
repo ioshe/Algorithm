85b7a92c59fa493bccc33735b368f73c198ded00
ioshe <148945226+ioshe@users.noreply.github.com>
2024-02-19 15:34:37 +0900

    [level 4] Title: 오프라인/온라인 판매 데이터 통합하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/4/131537. 오프라인／온라인 판매 데이터 통합하기/오프라인／온라인 판매 데이터 통합하기.sql
@@ -0,0 +1,20 @@
+select 
+    date_format(os.sales_date, '%Y-%m-%d') sales_date,
+    os.product_id,
+    os.user_id,
+    os.sales_amount
+from (
+    select 
+        sales_date,
+        product_id,
+        user_id,
+        sales_amount from online_sale
+    union all
+    select 
+        sales_date, 
+        product_id,
+        NULL,
+        sales_amount from offline_sale
+) os
+where os.sales_date like '2022-03%'
+order by sales_date, product_id, user_id;
