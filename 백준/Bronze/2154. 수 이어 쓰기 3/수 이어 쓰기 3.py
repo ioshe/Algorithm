cd8809c301607981ccf59b8aea66ea93b467bced
ioshe <148945226+ioshe@users.noreply.github.com>
2024-03-09 17:54:13 +0900

    [Bronze II] Title: 수 이어 쓰기 3, Time: 60 ms, Memory: 31596 KB -BaekjoonHub

--- 백준/Bronze/2154. 수 이어 쓰기 3/수 이어 쓰기 3.py
+++ 백준/Bronze/2154. 수 이어 쓰기 3/수 이어 쓰기 3.py
@@ -1,8 +1,10 @@
 # https://www.acmicpc.net/problem/2154
+def sol(n) :
+    result = ""
+    for i in range(1,n+1) :
+        result += str(i)
+    return result.find(str(n))
 
 n = int(input())
-result = ""
-for i in range(1,n+1) :
-    result += str(i)
+print(sol(n)+1)
 
-print(result.find(str(n))+1)
