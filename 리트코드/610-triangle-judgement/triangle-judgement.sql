e65922a5488fcd1ac1810b80756582c172b44fbd
ioshe <sdcard11291@gmail.com>
2024-03-10 18:38:57 +0900

    610-triangle-judgement

---
+++ 리트코드/610-triangle-judgement/triangle-judgement.sql
@@ -0,0 +1,12 @@
+# Write your MySQL query statement below
+SELECT 
+    x,
+    y,
+    z,
+    IF (
+        (x + y > z) AND (x + z > y) AND (y + z > x),
+        'Yes',
+        'No'
+    ) AS triangle
+FROM
+    Triangle;
