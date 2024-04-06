c5c446c3b27f9885408a79701b3bf62be1393396
ioshe <sdcard11291@gmail.com>
2024-04-06 21:52:19 +0900

    1625-group-sold-products-by-the-date

---
+++ 리트코드/1625-group-sold-products-by-the-date/group-sold-products-by-the-date.sql
@@ -0,0 +1,12 @@
+# Write your MySQL query statement below
+
+
+select
+    sell_date
+    , COUNT(DISTINCT product) AS num_sold
+    , GROUP_CONCAT(DISTINCT product ORDER BY product ASC SEPARATOR ',') AS products
+from
+    Activities
+group by
+    sell_date
+order by sell_date
