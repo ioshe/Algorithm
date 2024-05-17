fdf23583b9577ac98771310801a245b88847062f
ioshe <148945226+ioshe@users.noreply.github.com>
2024-05-17 23:14:35 +0900

    [Gold IV] Title: 이중 우선순위 큐, Time: 6752 ms, Memory: 337272 KB -BaekjoonHub

--- 백준/Gold/7662. 이중 우선순위 큐/이중 우선순위 큐.py
+++ 백준/Gold/7662. 이중 우선순위 큐/이중 우선순위 큐.py
@@ -1,78 +1,48 @@
-# https://www.acmicpc.net/problem/7662
-'''
-2
-7
-I 16
-I -5643
-D -1
-D 1
-D 1
-I 123
-D -1
-9
-I -45
-I 653
-D 1
-I -642
-I 45
-I 97
-D 1
-D -1
-I 333
-'''
-
 import heapq
 from sys import stdin
 
-def isEmpty(nums) :
-	# nums에 1인 데이터가 하나라도 있으면 비어있지 않은 것
-    for item in nums:
-        if item[1] > 0:
-            return False
-    return True
+def main():
+    index = 0
+    T = int(stdin.readline())
 
-T = int(stdin.readline())
+    for _ in range(T):
+        k = int(stdin.readline())
+        min_heap, max_heap = [], []
+        entry_finder = {}  # 현재 힙에 있는 원소의 존재 여부를 관리하는 사전
+        counter = 0
 
-for _ in range(T) :
-    N = int(stdin.readline())
-    # 연산을 나타내는 문자(‘D’ 또는 ‘I’)와 정수 n
-    min_heap = []
-    max_heap = []
-    entry_finder = {}
-    counter = 0
+        for _ in range(k):
+            operation, num = stdin.readline().split()
+            num = int(num)
     
-    for _n in range(N) :
-        co = stdin.readline().split()
-        op, value = co[0], int(co[1])
-        if op == "I" :
-            if value in entry_finder :
-                entry_finder[value] += 1
-                continue
-            heapq.heappush(min_heap,value)
-            heapq.heappush(max_heap,-value)
-            entry_finder[value] = 1
-            continue
-        elif op == 'D':
-            if not isEmpty(entry_finder.items()) :
-                if value == 1:  # Delete max
-                    while -max_heap[0] not in entry_finder or entry_finder[-max_heap[0]] < 1 :
-                        temp = -heapq.heappop(max_heap)
-                        if temp in entry_finder :
-                            del(entry_finder[temp])
-                    entry_finder[-max_heap[0]] -= 1
-                elif value == -1:  # Delete min
-                    while min_heap[0] not in entry_finder or entry_finder[min_heap[0]] < 1 :
-                        temp = heapq.heappop(min_heap)
-                        if temp in entry_finder :
-                            del(entry_finder[temp])
-                    entry_finder[min_heap[0]] -= 1
-        
-    if isEmpty(entry_finder.items()) :
-        print('EMPTY')
-    else :
-        while -max_heap[0] not in entry_finder or entry_finder[-max_heap[0]] < 1 :
-            heapq.heappop(max_heap)
-        while min_heap[0] not in entry_finder or entry_finder[min_heap[0]] < 1 :
-            heapq.heappop(min_heap)
-        print(f'{-max_heap[0]} {min_heap[0]}')
 
+            if operation == 'I':
+                heapq.heappush(min_heap, (num, counter))
+                heapq.heappush(max_heap, (-num, counter))
+                entry_finder[counter] = num
+                counter += 1
+            elif operation == 'D':
+                if num == 1:  # 최댓값 삭제
+                    while max_heap and max_heap[0][1] not in entry_finder:
+                        heapq.heappop(max_heap)
+                    if max_heap:
+                        _, idx = heapq.heappop(max_heap)
+                        entry_finder.pop(idx, None)
+                elif num == -1:  # 최솟값 삭제
+                    while min_heap and min_heap[0][1] not in entry_finder:
+                        heapq.heappop(min_heap)
+                    if min_heap:
+                        _, idx = heapq.heappop(min_heap)
+                        entry_finder.pop(idx, None)
+
+        while min_heap and min_heap[0][1] not in entry_finder:
+            heapq.heappop(min_heap)
+        while max_heap and max_heap[0][1] not in entry_finder:
+            heapq.heappop(max_heap)
+
+        if entry_finder:
+            print(f"{-max_heap[0][0]} {min_heap[0][0]}")
+        else:
+            print("EMPTY")
+
+main()
