f153692a355fa41e87edd6581950fa2dd31affd5
ioshe <148945226+ioshe@users.noreply.github.com>
2024-10-21 21:35:46 +0900

    [Bronze II] Title: 문자가 몇갤까, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/7600. 문자가 몇갤까/문자가 몇갤까.py
@@ -0,0 +1,15 @@
+while 1 :
+
+    s = input()
+
+    if s == '#' :
+
+        break
+
+    lst = [0] * 26	
+    for i in s.lower() :
+        if i.isalpha() :
+            lst[ord(i)-97] = 1
+        else :
+            continue
+    print(lst.count(1))
