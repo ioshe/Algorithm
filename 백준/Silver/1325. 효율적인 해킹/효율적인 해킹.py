47d702325cdcfb2082484d34763b8ae2f3cedc4e
ioshe <148945226+ioshe@users.noreply.github.com>
2024-11-19 17:25:45 +0900

    [Silver I] Title: 효율적인 해킹, Time: 10204 ms, Memory: 211392 KB -BaekjoonHub

--- 백준/Silver/1325. 효율적인 해킹/효율적인 해킹.py
+++ 백준/Silver/1325. 효율적인 해킹/효율적인 해킹.py
@@ -1,33 +1,41 @@
+# https://www.acmicpc.net/problem/1325
+
+# A가 B를 신뢰하는 경우에는 B를 해킹하면, A도 해킹할 수 있다
+# A를 해킹했을 때는 B를 해킹할 수 없다 
+# 따라서 방향성은 B -> A 가 된다.
+
+from sys import stdin
 from collections import deque
-import sys
-input = sys.stdin.readline
-def bfs(start):
-    q = deque()
-    q.append(start)
-    visit = [0 for _ in range(n + 1)]
-    visit[start] = 1
+
+N,M = map(int, stdin.readline().split())
+nums = list(map(lambda a : list(map(int,a.split())),stdin.readlines()))
+
+graph = [[] for i in range(N + 1)]
+for i,j in nums :
+    graph[j].append(i)
+
+def bfs(i) :
+    queue = deque([i])
+    visited = [False for _ in range(N+1)]
+    visited[i] = True
     cnt = 1
-    while q:
-        st = q.popleft()
-        for i in s[st]:
-            if visit[i] == 0:
-                visit[i] = 1
+    while queue :
+        current = queue.popleft()
+        for next in graph[current] :
+            if not visited[next] :
+                visited[next] = True
+                queue.append(next)
                 cnt += 1
-                q.append(i)
     return cnt
-n, m = map(int, input().split())
-s = [[] for i in range(n + 1)]
-for i in range(m):
-    a, b = map(int, input().split())
-    s[b].append(a)
+
 result = []
 max_cnt = 0
-for i in range(1, n + 1):
-    tmp = bfs(i)
-    if max_cnt == tmp:
-        result.append(i)
-    if max_cnt < tmp:
-        max_cnt = tmp
+for i in range(1,N+1) :
+    tmp = bfs(i) 
+    if max_cnt == tmp : 
+        result.append(i) 
+    elif max_cnt < tmp : 
+        max_cnt = tmp 
         result = []
         result.append(i)
 print(*result)
