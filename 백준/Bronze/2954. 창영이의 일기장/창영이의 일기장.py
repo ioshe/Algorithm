e75b3bf9977dfcdbc05680765e70058a81b1715d
ioshe <148945226+ioshe@users.noreply.github.com>
2024-11-23 18:17:17 +0900

    [Bronze II] Title: 창영이의 일기장, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/2954. 창영이의 일기장/창영이의 일기장.py
@@ -0,0 +1,15 @@
+sentence = input()
+
+i = 0
+
+vowels = ['a', 'e', 'i', 'o', 'u']
+
+while i < len(sentence):    # for -> range error
+
+    print(sentence[i], end='')
+
+    if sentence[i] in vowels:
+
+        i += 2
+
+    i += 1
