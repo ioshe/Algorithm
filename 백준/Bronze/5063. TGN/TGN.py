56a4660285bebf06d7c4fe526722220c81168c86
ioshe <148945226+ioshe@users.noreply.github.com>
2024-12-31 18:55:21 +0900

    [Bronze III] Title: TGN, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/5063. TGN/TGN.py
@@ -0,0 +1,17 @@
+case = int(input())
+
+for _ in range(case):
+
+    r, e, c = map(int, input().split())
+
+    if r > e - c:
+
+        print('do not advertise')
+
+    elif r == e - c:
+
+        print('does not matter')
+
+    else:
+
+        print('advertise')
