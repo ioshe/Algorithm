a2c9b6c47fad0aa2cb21811258e7ad5df9f1691d
ioshe <148945226+ioshe@users.noreply.github.com>
2025-01-05 22:06:14 +0900

    [Gold V] Title: 최소비용 구하기, Time: 2768 ms, Memory: 40388 KB -BaekjoonHub

---
+++ 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.py
@@ -0,0 +1,42 @@
+import heapq
+
+from sys import stdin
+from collections import defaultdict
+
+def dijkstra(graph, start ,end):
+    priority_queue = []
+    heapq.heappush(priority_queue, (0,start))
+    
+    distances = {node: float('inf') for node in range(1, N + 1)}
+    distances[start] = 0
+    
+    while priority_queue:
+        current_distance, current_node = heapq.heappop(priority_queue)
+        
+        # 이미 더 짧은 경로가 있으면 스킵
+        if current_distance > distances[current_node]:
+            continue
+        
+        for neighbor, weight in graph[current_node].items():
+            distance = current_distance + weight
+            
+            if distance < distances.get(neighbor,-float('inf')):
+                distances[neighbor] = distance
+                heapq.heappush(priority_queue, (distance, neighbor))
+    
+    return distances[end]
+
+
+N = int(stdin.readline())
+M = int(stdin.readline())
+
+graph = defaultdict(dict)
+for i in range(M):
+    start, end, cost = map(int, input().split())
+    # 간선 추가
+    if end not in graph[start] or graph[start][end] > cost:
+        graph[start][end] = cost
+
+start, end = map(int,stdin.readline().split())
+
+print(dijkstra(graph,start,end))
